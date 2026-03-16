542e0fe7939ba3a4ee0a53e85a7144be8d4bc8f0
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-16 15:41:15 -0300

    first commit

---
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿/* 
+  1. Ao iniciar o jogo, deve ser selecionada uma palavra aleatória à partir de uma lista.
+  2. O jogador poderá chutar a palavra secreta letra por letra, cada letra certa deverá ser apresentada,
+  assim como as letras erradas.
+  3. O jogador poderá cometer até cinco erros, caso erre pela quinta vez, ou acerte a palavra a partida
+  acaba.
+  4. Deve-se apresentar um desenho da forca sendo atualizado a cada erro. 
+*/
+
+namespace JogoDaForca.ConsoleApp;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello, World!");
+  }
+}
